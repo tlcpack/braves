ed6f0269da734074f5d337d83d4f4ab8fc5ad3f9
Taylor Cooke <taylorcooke@gmail.com>
2020-05-12 22:04:21 -0400

    working with class-based views. votes tallying

--- core/views.py
+++ core/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'core/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'core/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'core/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, q_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % q_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "core/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "core/results.html"
+
 
 def vote(request, q_id):
-    return HttpResponse("You're voting on question %s" % q_id)
+    question = get_object_or_404(Question, pk=q_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'core/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('core:results', args=(question.id,)))
